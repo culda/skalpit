9e4a9eb4cc48f2e07f04f1ceb18ba3cbdc4f0ae0
Bogdan Stanciu <Bogdan.Stanciu@axicorp.com>
2021-03-14 23:12:47 +0000

    more infra

--- infrastructure/scripts/bootstrap.sh
+++ infrastructure/scripts/bootstrap.sh
@@ -1,12 +1,16 @@
-#! /bin/bash
+#!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get -y upgrade 
 
 sudo apt-get install -y python3-pip
-sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-venv
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-venv -y
 
-git clone git@github.com:culda/skalpit.git
+# mai lipseste un python upgrade pentru ca default pe vm e 3.6.9
+
+echo "" >> .ssh/pk # aici merge deploy keyul
+
+git clone -q git@github.com:culda/skalpit.git
 cd skalpit
 python3 -m venv venv
 source venv/bin/activate
